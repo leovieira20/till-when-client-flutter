7f0e118de18b84e1cbe9db55e720fa245a987896
Leo <leovieira20@gmail.com>
2020-08-27 18:59:19 +0100

    fix: google sign in for ios

--- till_when/lib/domain/services/google_sign_in_service.dart
+++ till_when/lib/domain/services/google_sign_in_service.dart
@@ -24,14 +24,18 @@ class GoogleSignInService {
   }
 
   Future<UserCredential> _signInForMobile() async {
-    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    try {
+      final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-    final GoogleAuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
+      final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } on NoSuchMethodError {
+      return null;
+    }
   }
 }
